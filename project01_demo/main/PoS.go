e82958e1fad1a6b77173d5fddca86c1038883915
lingwu-hb <lingwu_hb@163.com>
2021-10-02 16:34:11 +0800

    PoS算法更新

--- project01_demo/main/PoS.go
+++ project01_demo/main/PoS.go
@@ -4,16 +4,17 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"strconv"
+	"math/rand"
 	"time"
 )
 
-var users []User
+var users []User = make([]User, 100)
+var userNum int
 
 //用户结构体
 type User struct {
 	coinNumber int64
-	address    string
+	address    string //用于身份验证
 	_durtime   time.Time
 	weight     int64 //用户权益值
 }
@@ -25,15 +26,51 @@ func (a User) ComputeWeight() {
 }
 
 // Initialize 实现User的初始化
-func (a User) Initialize() {
+func Initialize() {
 	//用户可以在这里指定初始信息
+	fmt.Println("请输入用户个数")
+	var n int
+	fmt.Scanln(&n)
+	i := 1
+	for ; i <= n; i++ {
+		var (
+			coinNumber int64
+			address    string
+		)
+		fmt.Println("请输入该用户的持币数，以及用户地址（随便一个字符串就行）")
+		fmt.Scanln(&coinNumber, &address)
+		users[i].coinNumber = coinNumber
+		users[i].address = address
+		users[i]._durtime = time.Now()
+		users[i].ComputeWeight()
+	}
+	userNum = i - 1
 }
 
-func ReturnIndex(users []User) int64 {
+// ReturnIndex 利用随机数返回一个幸运矿工的序号
+func ReturnIndex() int64 {
 	for _, user := range users {
 		user.ComputeWeight() //为每一个用户计算权益值
 	}
 	//随机数算法返回幸运的打包者
+	//计算总权重
+	var totalWeight int64
+	for i := 1; i <= userNum; i++ {
+		totalWeight += users[i].weight
+	}
+	var portion []float64 = make([]float64, userNum+1) //记录每个用户的比例（portion）
+	for i := 1; i <= userNum; i++ {
+		// 将每个用户所占的比例计算出来，然后再加上之前的用户的比例（最终所有用户的比例会占满[0, 1]）
+		portion[i] = float64(users[i].weight)/float64(totalWeight) + portion[i-1]
+	}
+	rand.Seed(time.Now().Unix())
+	var x = rand.Float64()
+	for i := 1; i <= userNum; i++ {
+		if x > portion[i-1] && x < portion[i] {
+			return int64(i)
+		}
+	}
+	return 0
 }
 
 //区块的结构
@@ -46,20 +83,19 @@ type Block struct {
 	Data       string //区块中的交易信息
 }
 
+var BlockList []Block = make([]Block, 100)
+
 //创世区块
-func GenerateFirstBlock(data string) Block {
+func GenerateFirstBlock(data string, cut int, validator string) {
 	var firstblock Block
 	/*add your code here*/
-	firstblock.PreHash = "0"
-	firstblock.Data = data
-
-	var sha = sha256.New()
-	sha.Write([]byte(firstblock.Data))
-	hashed := sha.Sum(nil)
-	firstblock.MerkleHash = hex.EncodeToString(hashed)
-
+	firstblock.Index = cut
+	firstblock.Validator = validator
+	firstblock.PreHash = ""                                         //默认初始值，为空
 	firstblock.TimeStamp = time.Now().Format("2006-01-02 15:04:05") //格式化
-	return firstblock
+	firstblock.Data = data
+	firstblock.MerkleHash = GenerationHashValue(firstblock)
+	BlockList[cut] = firstblock
 }
 
 //计算区块的哈希值
@@ -67,7 +103,7 @@ func GenerationHashValue(block Block) string {
 	//请仿照比特币的计算方式计算哈希
 	var hashdata string
 	/*add your code here*/
-	hashdata = block.PreHash + block.MerkleHash + block.TimeStamp + strconv.Itoa(block.Diff) + block.Data + strconv.Itoa(block.Nonce)
+	hashdata = string(block.Index) + block.Validator + block.PreHash + block.TimeStamp + block.Data
 	var sha = sha256.New()
 	sha.Write([]byte(hashdata))
 	hashed := sha.Sum(nil)
@@ -83,22 +119,36 @@ func GenerateNextBlock(data string, oldBlock Block, cut int, validator string) {
 	newBlock.PreHash = GenerationHashValue(oldBlock) //先算出上个区块的哈希，然后再赋值
 	newBlock.TimeStamp = time.Now().Format("2006-01-02 15:04:05")
 	newBlock.Data = data
-
-	hashdata := string(newBlock.Index) + newBlock.Validator + newBlock.PreHash + newBlock.TimeStamp + newBlock.Data
-	var sha = sha256.New()
-	sha.Write([]byte(hashdata))
-	hashed := sha.Sum(nil)
-	newBlock.MerkleHash = hex.EncodeToString(hashed)
+	newBlock.MerkleHash = GenerationHashValue(newBlock)
+	BlockList[cut] = newBlock
 }
 
-func Print_(block Block) {
+func Print_() {
+	fmt.Println(users)
 	fmt.Println(BlockList)
 }
-
-var BlockList []Block
 
 //main函数
 func main() {
 	//初始化每一个用户的前两个数据元素
-
+	Initialize()
+	//Print_()
+	cut := 1
+	validator := ReturnIndex()
+	GenerateFirstBlock("hanbo->xiongyuhan 100bitcoin", cut, users[validator].address)
+	for {
+		var pos int
+		fmt.Println("输入0以退出循环")
+		fmt.Scanln(&pos)
+		if pos == 0 {
+			break
+		}
+		var data string
+		fmt.Println("请输入交易信息")
+		fmt.Scanln(&data)
+		cut++
+		validator := ReturnIndex()
+		GenerateNextBlock(data, BlockList[cut-1], cut, users[validator].address)
+	}
+	Print_()
 }
--- project01_demo/main/test.go
+++ project01_demo/main/test.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+var a []int = make([]int, 10)
+
 func main() {
-	fmt.Println("hanbpo")
-	a := time.Now()
-	var c float64
-	c = 25.6
-	b := time.Now().Sub(a).Microseconds()
-	d := float64(b) * c
-	fmt.Println(b)
-	fmt.Println(d)
+	for i := range a {
+		fmt.Println(i)
+	}
 }
